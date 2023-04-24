76e678ed5d26b0945ed42ef09bf08ab3f6cacf21
Ruben Peña <rpgimenez2@gmail.com>
2023-04-24 20:46:53 +0200

    crudLARAVELangular

---
+++ angularLARAVEL/backend/routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\PersonController;
+
+Route::prefix('person')->group(function () {
+    Route::get('/', [PersonController::class, 'getAll']);
+    Route::post('/', [PersonController::class, 'create']);
+    Route::delete('/{id}', [PersonController::class, 'delete']);
+    Route::get('/{id}', [PersonController::class, 'get']);
+    Route::put('/{id}', [PersonController::class, 'update']);
+});
